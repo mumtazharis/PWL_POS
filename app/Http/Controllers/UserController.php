c7059990a681a0fcf7267a07aa0f5b79934cd178
mumtazharis <ahmadmumtazhrs@gmail.com>
2024-03-13 13:34:06 +0700

    Jobsheet 4 Praktikum 2

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,16 +9,18 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     public function index(){
-        $data = [
-            'level_id' => 2,
-            'username' => 'namager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345')
+        // $data = [
+        //     'level_id' => 2,
+        //     'username' => 'namager_tiga',
+        //     'nama' => 'Manager 3',
+        //     'password' => Hash::make('12345')
             
-        ];
-        UserModel::create($data);
+        // ];
+        // UserModel::create($data);
 
-        $user = UserModel::all();
+        $user = UserModel::findOr(20, ['username', 'nama'], function(){
+            abort(404);
+        });
         return view('user', ['data'=>$user]);
     }
 }
