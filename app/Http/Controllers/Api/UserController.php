4db6e988cb0990a3ae312794455011ec119a7a77
mumtazharis <ahmadmumtazhrs@gmail.com>
2024-05-05 16:34:07 +0700

    Jobsheet 10 Tugas

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\UserModel;
+
+class UserController extends Controller
+{
+    public function index(){
+        return UserModel::all();
+    }
+
+    public function store(Request $request){
+        
+        $user = UserModel::create([
+            'username'  => $request->username,
+            'nama'      => $request->nama,
+            'password'  => bcrypt($request->password),
+            'level_id'  => $request->level_id
+        ]);
+        return response()->json($user, 201);
+    }
+
+    public function show($user){
+        return UserModel::find($user);
+    }
+
+    public function update(Request $request, UserModel $user){
+        $user->update($request->all());
+        return $user;
+    }
+
+    public function destroy(UserModel $user){
+        $user->delete();
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Data terhapus',
+        ]);
+    }
+}
